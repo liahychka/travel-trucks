6d21dde22741e4c567c7effa4c281dd76dc36701
liahychka <157300297+liahychka@users.noreply.github.com>
2025-01-30 14:35:27 +0100

    перемикання навігації

--- src/components/NavigationDetails/NavigationDetails.jsx
+++ src/components/NavigationDetails/NavigationDetails.jsx
@@ -1,12 +1,67 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import FeaturesCampers from "../FeaturesCampers/FeaturesCampers";
+import ReviewsCampers from "../ReviewsCampers/ReviewsCampers";
+import { selectCamperById } from "../../redux/campers/selectors";
+import { useParams } from "react-router-dom";
+
 const NavigationDetails = () => {
+  const { id } = useParams();
+  const camper = useSelector((state) => selectCamperById(state, id));
+
+  const [activeTab, setActiveTab] = useState("features");
+
+  if (!camper) {
+    return <h2>Camper not found</h2>;
+  }
+
   return (
     <div>
-          <ul>
-              <li>Features</li>
-              <li>Reviews</li>
-          </ul>
-    </div>
-  )
-}
+      <ul>
+        <li onClick={() => setActiveTab("features")}>
+          Features
+        </li>
+        <li onClick={() => setActiveTab("reviews")}>
+          Reviews
+        </li>
+      </ul>
 
-export default NavigationDetails
+      {activeTab === "features" && (
+        <FeaturesCampers
+          transmission={camper.transmission}
+          engine={camper.engine}
+          kitchen={camper.kitchen ? "kitchen" : null}
+          AC={camper.AC ? "AC" : null}
+          water={camper.water ? "water" : null}
+          gas={camper.gas ? "gas" : null}
+          microwave={camper.microwave ? "microwave" : null}
+          refrigerator={camper.refrigerator ? "refrigerator" : null}
+          radio={camper.radio ? "radio" : null}
+          TV={camper.TV ? "TV" : null}
+          bathroom={camper.bathroom ? "bathroom" : null}
+          consumption={camper.consumption}
+          tank={camper.tank}
+          height={camper.height}
+          width={camper.width}
+          length={camper.length}
+          form={camper.form}
+        />
+      )}
+
+      {activeTab === "reviews" && (
+        <div>
+          {camper.reviews.map((review, index) => (
+            <ReviewsCampers
+              key={index}
+              reviewer_name={review.reviewer_name}
+              reviewer_rating={review.reviewer_rating}
+              comment={review.comment}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavigationDetails;
--- src/pages/CampersDetailsPage/CampersDetailsPage.jsx
+++ src/pages/CampersDetailsPage/CampersDetailsPage.jsx
@@ -1,11 +1,9 @@
 import css from "./CampersDetailsPage.module.css";
 import { useParams } from "react-router-dom";
 import CatalogItem from "../../components/CatalogItem/CatalogItem";
-import FeaturesCampers from "../../components/FeaturesCampers/FeaturesCampers";
 import { useSelector } from "react-redux";
 import { selectCamperById } from "../../redux/campers/selectors.js";
 import FormBooking from "../../components/FormBooking/FormBooking.jsx";
-import ReviewsCampers from "../../components/ReviewsCampers/ReviewsCampers.jsx";
 import NavigationDetails from "../../components/NavigationDetails/NavigationDetails.jsx";
 
 const CampersDetailsPage = () => {
@@ -29,33 +27,6 @@ const CampersDetailsPage = () => {
           gallery={camper.gallery}
         />
         <NavigationDetails />
-        <FeaturesCampers
-          transmission={camper.transmission}
-          engine={camper.engine}
-          kitchen={camper.kitchen ? "kitchen" : null}
-          AC={camper.AC ? "AC" : null}
-          water={camper.water ? "water" : null}
-          gas={camper.gas ? "gas" : null}
-          microwave={camper.microwave ? "microwave" : null}
-          refrigerator={camper.refrigerator ? "refrigerator" : null}
-          radio={camper.radio ? "radio" : null}
-          TV={camper.TV ? "TV" : null}
-          bathroom={camper.bathroom ? "bathroom" : null}
-          consumption={camper.consumption}
-          tank={camper.tank}
-          height={camper.height}
-          width={camper.width}
-          length={camper.length}
-          form={camper.form}
-        />
-        {camper.reviews.map((review, index) => (
-          <ReviewsCampers
-            key={index}
-            reviewer_name={review.reviewer_name}
-            reviewer_rating={review.reviewer_rating}
-            comment={review.comment}
-          />
-        ))}
         <FormBooking />
       </div>
     </div>
