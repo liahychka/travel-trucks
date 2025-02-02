928c2f0413ff674f850b6aa071722c2769ee426c
liahychka <157300297+liahychka@users.noreply.github.com>
2025-02-02 20:27:53 +0100

    стилі фільтрація

--- src/components/FilterComponent/FilterComponent.jsx
+++ src/components/FilterComponent/FilterComponent.jsx
@@ -29,118 +29,157 @@ const FilterComponent = () => {
   };
 
   return (
-    <div>
-      <h3>Filters</h3>
-      
-      <div>
-        <label>Location</label>
+    <div className={css.boxFilter}>
+      <div className={css.divLocation}>
+        <label className={css.location}>Location</label>
         <input 
           type="text" 
           value={location}
           onChange={handleLocationChange}
-          placeholder="Search by location"
-        />
-      </div>
+            placeholder="City"
+            className={css.input}
+              />     
+          </div>
+          
+     <h4 className={css.nameFilter}>Filters</h4>
 
-      <h3>Vehicle equipment</h3>
-      <ul>
-        <li>
-          <input 
+          <h4 className={css.vehicle}>Vehicle equipment</h4>
+    <ul className={css.listVehicle}>
+        <li className={css.itemFilter} >
+        <label className={css.label}>
+            <input 
             type="checkbox" 
             name="ac"
             checked={equipment.ac}
             onChange={handleEquipmentChange}
-          />
-          <svg width="32" height="32">
+            className={css.hiddenCheckbox}
+            />
+            <svg width="32" height="32" className={equipment.ac ? css.activeIcon : ""}>
             <use href={`${icons}#icon-ac`} />
-          </svg>
+            </svg>
+            <span>AC</span>
+        </label>
         </li>
-        <li>
-          <input 
-            type="checkbox" 
-            name="kitchen"
-            checked={equipment.kitchen}
-            onChange={handleEquipmentChange} 
-          />
-          <svg width="32" height="32">
-            <use href={`${icons}#icon-kitchen`} />
-          </svg>
-        </li>
-        <li>
-          <input 
-            type="checkbox" 
-            name="tv"
-            checked={equipment.tv}
-            onChange={handleEquipmentChange} 
-          />
-          <svg width="32" height="32">
-            <use href={`${icons}#icon-tv`} />
-          </svg>
-        </li>
-        <li>
-          <input 
-            type="checkbox" 
-            name="bathroom"
-            checked={equipment.bathroom}
-            onChange={handleEquipmentChange} 
-          />
-          <svg width="32" height="32">
-            <use href={`${icons}#icon-bathroom`} />
-          </svg>
-              </li>
-              <li>
-          <input 
-            type="checkbox" 
-            name="automatic"
-            checked={equipment.automatic}
-            onChange={handleEquipmentChange} 
-          />
-        <svg width="32" height="32">
-            <use href={`${icons}#icon-automatic`} />
-          </svg>
-        </li>
-      </ul>
+  <li className={css.itemFilter}>
+    <label className={css.label}>
+      <input 
+        type="checkbox" 
+        name="kitchen"
+        checked={equipment.kitchen}
+        onChange={handleEquipmentChange} 
+        className={css.hiddenCheckbox} 
+      />
+      <svg width="32" height="32" className={equipment.kitchen ? css.activeIcon : ""}>
+        <use href={`${icons}#icon-kitchen`} />
+        </svg>
+        <span>Kitchen</span>
+    </label>
+  </li>
 
-      <h3>Vehicle type</h3>
-      <ul>
-        <li>
-          <input 
-            type="radio" 
-            name="vehicleType" 
-            value="panelTruck" 
-            checked={vehicleType === "panelTruck"}
-            onChange={handleVehicleTypeChange}
-          />
-          <svg width="32" height="32">
-            <use href={`${icons}#icon-van`} />
-          </svg>
-        </li>
-        <li>
-          <input 
-            type="radio" 
-            name="vehicleType" 
-            value="alcove" 
-            checked={vehicleType === "alcove"}
-            onChange={handleVehicleTypeChange}
-          />
-          <svg width="32" height="32">
-            <use href={`${icons}#icon-alcove`} />
-          </svg>
-        </li>
-        <li>
-          <input 
-            type="radio" 
-            name="vehicleType" 
-            value="fullyIntegrated" 
-            checked={vehicleType === "fullyIntegrated"}
-            onChange={handleVehicleTypeChange}
-          />
-          <svg width="32" height="32">
-            <use href={`${icons}#icon-fullyIntegrated`} />
-          </svg>
-        </li>
-      </ul>
+  <li className={css.itemFilter}>
+    <label className={css.label}>
+      <input 
+        type="checkbox" 
+        name="tv"
+        checked={equipment.tv}
+        onChange={handleEquipmentChange} 
+        className={css.hiddenCheckbox} 
+      />
+      <svg width="32" height="32" className={equipment.tv ? css.activeIcon : ""}>
+        <use href={`${icons}#icon-tv`} />
+        </svg>
+       <span>TV</span>                   
+    </label>
+  </li>
 
+        <li className={css.itemFilter}>
+            <label className={css.label}>
+            <input 
+                type="checkbox" 
+                name="bathroom"
+                checked={equipment.bathroom}
+                onChange={handleEquipmentChange} 
+                className={css.hiddenCheckbox} 
+            />
+            
+            <svg width="32" height="32" className={equipment.bathroom ? css.activeIcon : ""}>
+                <use href={`${icons}#icon-bathroom`} />
+                          </svg>
+            <span>Bathroom</span>
+            </label>
+        </li>
+
+        <li className={css.itemFilter}>
+            <label className={css.label}>
+            <input 
+                type="checkbox" 
+                name="automatic"
+                checked={equipment.automatic}
+                onChange={handleEquipmentChange} 
+                className={css.hiddenCheckbox} 
+            />
+            
+            <svg width="32" height="32" className={equipment.automatic ? css.activeIcon : ""}>
+                <use href={`${icons}#icon-automatic`} />
+                          </svg>
+            <span>Automatic</span>
+            </label>
+        </li>
+        </ul>
+
+        <h3 className={css.nameVehicleType}>Vehicle type</h3>
+        <ul className={css.listVehicle}>
+        <li className={css.itemFilter}>
+            <label className={css.label}>
+            <input 
+                type="radio" 
+                name="vehicleType" 
+                value="panelTruck" 
+                checked={vehicleType === "panelTruck"}
+                onChange={handleVehicleTypeChange}
+                className={css.hiddenCheckbox} 
+            />
+            <svg width="32" height="32" className={vehicleType === "panelTruck" ? css.activeIcon : ""}>
+                <use href={`${icons}#icon-van`} />
+                          </svg>
+            <span>Van</span>
+            </label>
+        </li>
+
+        <li className={css.itemFilter}>
+            <label className={css.label}>
+            <input 
+                type="radio" 
+                name="vehicleType" 
+                value="alcove" 
+                checked={vehicleType === "alcove"}
+                onChange={handleVehicleTypeChange}
+                className={css.hiddenCheckbox} 
+            />
+            <svg width="32" height="32" className={vehicleType === "alcove" ? css.activeIcon : ""}>
+                <use href={`${icons}#icon-alcove`} />
+                          </svg>
+            <span>Alcove</span>
+            </label>
+        </li>
+
+        <li className={css.itemFilter}>
+            <label className={css.label}>
+            <input 
+                type="radio" 
+                name="vehicleType" 
+                value="fullyIntegrated" 
+                checked={vehicleType === "fullyIntegrated"}
+                onChange={handleVehicleTypeChange}
+                className={css.hiddenCheckbox} 
+            />
+            <svg width="32" height="32" className={vehicleType === "fullyIntegrated" ? css.activeIcon : ""}>
+                <use href={`${icons}#icon-fullyIntegrated`} />
+                          </svg>
+            <span>Fully Integrated</span>
+            </label>
+        </li>
+        </ul>
       <button onClick={handleSearch}>Search</button>
     </div>
   );
